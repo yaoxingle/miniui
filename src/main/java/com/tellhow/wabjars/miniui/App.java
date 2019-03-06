698f58e61aaed7c68e8f5698aed8339147db804c
yaoxl <286462534@qq.com>
2019-03-06 14:29:24 +0800

    第一次提交

---
+++ src/main/java/com/tellhow/wabjars/miniui/App.java
@@ -0,0 +1,13 @@
+package com.tellhow.wabjars.miniui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
